be3ded3fd123ed8794cb38c53f887c18b6a03034
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-05-24 03:20:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,31 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-main" {
    Name = join("", [var.coid, "-SecVPC"])
   }
 }
+
+resource "aws_internet_gateway" "main_igw" {
+  depends_on = [aws_ec2_transit_gateway.main_tgw,aws_internet_gateway.main_igw]
+  vpc_id = var.vpc_cidr
+  tags = {
+    Name = join("", [var.coid, "-IGW"])
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+  
+  
+  tags = {
+    Name = ("Public-rt")
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  depends_on = [aws_route_table.public_rt]
+  subnet_id      = "subnet-0c8733bef4b4d1945"
+  route_table_id = aws_route_table.public_rt.id
+}
