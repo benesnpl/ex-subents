338cc18ed5a506ea210f76017cb87992f955e0ce
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-05-24 03:43:21 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,3 +14,118 @@ variable "sub_tgw" {
   type = list(string)
   default = ["subnet-0ba95bcefb564a33a","subnet-009eaf0ed0f70b5ff"]
 }
+
+variable "rules_inbound_public_sg" {
+  default = [
+    {
+      port = 0
+      proto = "-1"
+      cidr_block = ["0.0.0.0/0"]
+    }
+    ]
+}
+
+variable "rules_outbound_public_sg" {
+  default = [
+    {
+      port = 0
+      proto = "-1"
+      cidr_block = ["0.0.0.0/0"]
+    }
+    ]
+}
+
+variable "rules_inbound_private_sg" {
+  default = [
+    {
+      port = 0
+      proto = "-1"
+      cidr_block = ["10.0.0.0/8","192.168.0.0/16","172.16.0.0/12","100.70.0.0/15"]
+    }
+    ]
+}
+
+variable "rules_outbound_private_sg" {
+  default = [
+    {
+      port = 0
+      proto = "-1"
+      cidr_block = ["0.0.0.0/0"]
+    }
+    ]
+}
+
+variable "rules_inbound_mgmt_sg" {
+  default = [
+  
+    {
+      port = 22
+      proto = "tcp"
+      cidr_block = ["10.159.94.0/23"]
+    },
+	
+	{
+      port = 443
+      proto = "tcp"
+      cidr_block = ["10.159.94.0/23"]
+    },
+	
+	
+	{
+      port = 161
+      proto = "udp"
+      cidr_block = ["10.159.94.0/23"]
+    },
+	
+	{
+      port = 8
+      proto = "icmp"
+      cidr_block = ["10.159.94.0/23"]
+    },
+	
+	{
+      port = 22
+      proto = "tcp"
+      cidr_block = ["100.70.0.0/15"]
+    },
+	
+	{
+      port = 443
+      proto = "tcp"
+      cidr_block = ["100.70.0.0/15"]
+    },
+	
+	
+	{
+      port = 161
+      proto = "udp"
+      cidr_block = ["100.70.0.0/15"]
+    },
+	
+	{
+      port = 8
+      proto = "icmp"
+      cidr_block = ["100.70.0.0/15"]
+    }
+	
+	
+    ]
+}
+
+variable "rules_outbound_mgmt_sg" {
+  default = [
+    {
+      port = 0
+      proto = "-1"
+      cidr_block = ["0.0.0.0/0"]
+    }
+    ]
+}
+
+variable "il_external" {
+	default = null
+}
+
+variable "fl_external" {
+	default = null
+}
