9c7beeb0eb078995d65c2646ca752b269cea98e7
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-05-24 03:11:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_ec2_transit_gateway" "main_tgw" {
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-main" {
   depends_on = [aws_ec2_transit_gateway.main_tgw]
-  subnet_ids         = [var.sub_tgw]
+  subnet_ids         = ["subnet-009eaf0ed0f70b5ff","subnet-0ba95bcefb564a33a"]
   transit_gateway_id = aws_ec2_transit_gateway.main_tgw.id
   vpc_id             = var.vpc_cidr
   appliance_mode_support = "enable"
